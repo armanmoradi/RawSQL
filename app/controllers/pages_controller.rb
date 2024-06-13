988590a1750c24f8b603130fdc9ee8f6cb59ab26
armanmoradi <armanmoradi8@gmail.com>
2024-06-14 01:09:10 +0330

    add raw sql to page controller and create PostsAndCommends model for read hash answer

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,13 @@
 class PagesController < ApplicationController
   def home
+    @records = ActiveRecord::Base.connection.exec_query("
+      SELECT title, content, created_at, 'Post' as record_type
+      FROM posts
+      UNION
+      SELECT NULL as title, comments.content, comments.created_at, 'Comment' as record_type
+      FROM comments
+      INNER JOIN posts ON posts.id = comments.post_id
+      ORDER BY created_at ASC, content ASC;
+    ").map { |raw| PostsAndCommends.new(raw)}
   end
 end
